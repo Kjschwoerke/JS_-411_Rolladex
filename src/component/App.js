99f787b23a2e59f0255c70401a6d62ec222310b9
kjschwoerke <kjschwoerke@yahoo.com>
2020-01-15 19:32:37 -0600

    added styling 1-15-2020

--- src/component/App.js
+++ src/component/App.js
@@ -31,7 +31,7 @@ render(){
     const {users} = this.state
     let userPeople = users
     let people = userPeople.results
-
+    
     console.log(people)
     
     if (userPeople.length === 0) {
@@ -39,16 +39,23 @@ render(){
     }
 
      return (
+       
        <div className = "App">
-         <div>The Data Is Loaded</div>
+         <h1>User Contact Information</h1>
          {people.map(person => (
+          <ul>
+           <li  key = {person.id}>
            <Expanded
            image={person.picture.large}
            firstName={person.name.first}
            lastName={person.name.last}
            email={person.email}
+           city={person.location.city}
+           state={person.location.state}
+           country={person.location.country}
            />
-            // <div>{person.name.last}</div>
+           </li>
+          </ul>
          ))}
       </div>
        )
--- src/component/DefaultInfo.js
+++ src/component/DefaultInfo.js
@@ -3,40 +3,43 @@ import React from 'react'
 class DefaultInfo extends React.Component {
     state = {
         isExpanded: true,
-        Expanded: 'No'
     }
 
     Expanded() {
         let isExpanded = this.state.isExpanded
         this.setState({isExpanded: !isExpanded})
-        // if (isExpanded) {
-        // this.setState({Expanded: 'Yes'}) 
-        // }else{this.setState({Expanded: 'No'})}
       }
 
 render(){
     {if (this.state.isExpanded){
     return (
-        <li id ='userCard' key = {this.props.id}>
-        <img id ="userImg" src={this.props.image}></img>
+
+        <section id ='userCard'>
+        <img id ="userImg" src={this.props.image} alt ='someImage'></img>
         <br/>
-        <span id ="userName">{this.props.firstName} {this.props.lastName}</span>
-        <br/>
-        <div id ="userEmail">{this.props.email}</div>
+        <span id ="userName"><h2>{this.props.firstName} {this.props.lastName}</h2></span>
+        <div id ="userEmail">User Email: {this.props.email}</div>
         
         <button onClick = {() => this.Expanded()} id="expandButton">Click to expand info for "{this.props.firstName}".</button>
-        </li>
+        </section>
+
       )} else if (this.state.isExpanded === false){
-          return (
-            <li id ='userCard' key = {this.props.id}>
-            <img id ="userImg" src={this.props.image}></img>
-            <br/>
-            <span id ="userName">{this.props.firstName} {this.props.lastName}</span>
-            <br/>
-            <div id ="userEmail">{this.props.email}</div>
-            <div>and some additional stuff</div>
-            <button onClick = {() => this.Expanded()} id="unExpandButton">Click to minimize info for "{this.props.firstName}".</button>
-            </li>
+      return (
+<>
+        <section id ='userCard'>
+        <img id ="userImg" src={this.props.image} alt ='someImage'></img>
+        <br/>
+        <span id ="userName"><h2>{this.props.firstName} {this.props.lastName}</h2></span>
+        </section>
+        <p id ="userEmail">User Email: {this.props.email}</p>
+        <p id ="userCity">City: {this.props.city}</p>
+        <p id ="userState">State: {this.props.state}</p>
+        <p id ="userCountry">Country: {this.props.country}</p>
+        
+
+        <button onClick = {() => this.Expanded()} id="unExpandButton">Click to minimize info for "{this.props.firstName}".</button>
+        
+</>
           )}
       }
     }
